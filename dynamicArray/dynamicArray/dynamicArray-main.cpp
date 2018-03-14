86a657888d3a0422abf6cc5f14900428643e0891
tuanlestudy <tuanlestudy@gmail.com>
2018-03-13 21:09:05 -0700

    update

--- dynamicArray/dynamicArray/dynamicArray-main.cpp
+++ dynamicArray/dynamicArray/dynamicArray-main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+#include "dynamicArray.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int size = 0;
+    int * myArray;
+    
+    //array_constructor( myArray, size=5);
+    myArray = array_constructor(myArray, size);
+    
+    printTest(myArray, size);
+    
+    //array_resize( myArray, currSize, newSize);
+    int currSize = size;
+    int newSize = 0;
+    myArray = array_resize(myArray, currSize, newSize);
+    currSize = newSize;
+    printTest(myArray, currSize);
+    
+    //array_set( myArray, currSize);
+    array_set(myArray, currSize);
+    
+    printTest(myArray, currSize);
+    
+    mysort(myArray, currSize, my_less_equal);
+    
+    printTest(myArray, currSize);
+    
+    mysort(myArray, currSize, my_greater_equal);
+    
+    printTest(myArray, currSize);
+    
+    array_destructor(myArray);
+    
     return 0;
 }
--- dynamicArray/dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray/dynamicArray.h
@@ -9,5 +9,18 @@
 #ifndef dynamicArray_h
 #define dynamicArray_h
 
+const int firstValue = 0;
+const int secondValue = 1;
 
+int * array_constructor(int *  &intPtr, int &size );
+int * array_resize(int * &intPtr, int& currSize, int& newSize);
+void array_destructor(int * &intPtr);
+void array_set(int* &intPtr, int &size);
+
+void mysort( int* &intPtr, int size, bool (* comp)(int&, int&) );
+bool my_less_equal(int& x, int & y);         //return true if x<=y, false otherwise.
+bool my_greater_equal(int& x, int & y );    //return true if x>=y, false otherwise
+
+
+void printTest(int * &intPtr, int &size);
 #endif /* dynamicArray_h */
