e3aca2283948629a67a0597fc70f2954a0d8c021
Le Tuan <kai@kais-MacBook-Pro.local>
2018-04-02 18:35:04 -0700

    update

---
+++ 2018.sfsu.csc340.fileIOs_wordPairs/fileIOs_wordPairs.h
@@ -0,0 +1,17 @@
+#ifndef fileOs_wordPairs
+#define fileOs_wordPairs
+
+// Header file contents
+bool sentenceSplitter( string& fname, vector<string>& sentences);
+/*
+period: .,
+question mark: ?
+period + double quotation mark: ." 
+question mark + double quotation mark: ?"
+*/
+bool wordpairMapping( vector<string>& sentences, map< pair<string,string>, int>&wordpairFreq_map);
+bool freqWordpairMmap(map< pair<string,string>, int> &wordpairFreq_map, multimap<int, pair<string,string> > &freqWordpair_mmap );
+void printWordpairs(multimap<int, pair<string, string> > &freqWordpair_multimap, string outFname, int topCnt, int botCnt);
+
+
+#endif
